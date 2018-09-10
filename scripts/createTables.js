99b73daeb7155df3cb2ddf5a182e7c0d81ee2dce
gitachyut <linkinakd72@gmail.com>
2018-09-10 17:48:06 +0530

    create table script added

--- scripts/createTables.js
+++ scripts/createTables.js
@@ -1,16 +1,17 @@
-const exec = require('child_process').exec, child;
-const testscript = exec('sh myscript.sh /directory');
+const exec = require('child_process').exec;
+const fs = require('fs')
 
-testscript.stdout.on('data', function(data){
-    console.log(data); 
-});
 
-testscript.stderr.on('data', function(data){
-    console.log(data);
-    // triggerErrorStuff(); 
-});
+fs.readdirSync(__dirname + '/../config/tables/').forEach((file) => {
+  const testscript = exec(`aws dynamodb create-table --cli-input-json file://${__dirname}/../config/tables/${file}`);
+  testscript.stdout.on('data', function(data){
+      console.log(data); 
+  });
 
-fs.readdirSync(__dirname + '../config/tables/').forEach((file) => {
-  require(`./controllers/${file.substr(0, file.indexOf('.'))}`)(app,passport);
+  testscript.stderr.on('data', function(data){
+      console.log(data);
+      // triggerErrorStuff(); 
+  });
+
 });
 
